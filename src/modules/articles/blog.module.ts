30d28b94f3cbf26cf89765a312715bc84d5285c5
rahatalmas <rahat.almas5599@gmail.com>
2025-12-04 18:23:13 +0600

    blog-module

---
+++ src/modules/articles/blog.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { BlogsService } from './blog.service';
+import { BlogsController } from './blog.controller';
+import { BlogRepository } from './blog.repository';
+import { MongoModule } from '../../common/modules/mongo.module';
+
+@Module({
+  imports:[MongoModule],
+  controllers: [BlogsController],
+  providers: [BlogsService, BlogRepository],
+  exports:[BlogRepository]
+})
+export class BlogsModule {}
