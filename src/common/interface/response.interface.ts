31e1dcc43fdcc9f8a53ecedfaa0c065bfc90e6d0
rahatalmas <rahat.almas5599@gmail.com>
2025-12-04 18:16:55 +0600

    response interface

---
+++ src/common/interface/response.interface.ts
@@ -0,0 +1,13 @@
+//response model for seccessful execution
+export class ResponseInterface{
+    status:string
+    message:string
+    data?:any
+    accessToken?:string
+    constructor({message,data,accessToken}:{message:string,data?:any,accessToken?:string}){
+        this.status = "success",
+        this.message = message,
+        this.data = data
+        this.accessToken = accessToken
+    }
+}
