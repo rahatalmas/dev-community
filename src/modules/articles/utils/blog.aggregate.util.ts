04e5a7bb6067e248fd6415b9255f8e81bbd60fc7
rahatalmas <rahat.almas5599@gmail.com>
2025-12-04 18:21:24 +0600

    aggregation query utils

---
+++ src/modules/articles/utils/blog.aggregate.util.ts
@@ -0,0 +1,19 @@
+export const blogProject = {
+  status: 1,
+  title: 1,
+  content: 1,
+  summary: 1,
+  tags: 1,
+  likesCount: 1,
+  commentsCount: 1,
+
+  "author.fullname": 1,
+  "author.username": 1,
+  "author.dp_uri": 1,
+  "comments._id": 1,
+  "comments.comment": 1,
+  "comments.user_id": 1,
+  "comments.user.fullname": 1,
+  "comments.user.username": 1,
+  "comments.user.dp_uri": 1
+};
