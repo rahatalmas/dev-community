786de61adb991a1efe68a386744366f89c8d8513
rahatalmas <rahat.almas5599@gmail.com>
2025-12-04 18:14:09 +0600

    global exception filter for error catching

---
+++ src/common/exception-filter/app.exception.ts
@@ -0,0 +1,32 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+
+//global exception filter for catching all errors and exceptions
+@Catch()
+export class GlobalExceptionFilter implements ExceptionFilter {
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : 500;
+
+    const message =
+      exception instanceof HttpException
+        ? exception.getResponse()
+        : 'Internal server error';
+
+    response.status(status).json({
+      status: 'error',
+      message,
+      path: request.url,
+    });
+  }
+}
